9aebd51e77627afe8c4c8ff87e931a9c996d115e
YounesDjelloul <younesdjelloul14@gmail.com>
2024-12-25 01:48:20 +0800

    adding tweet length as an extra feature

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 import streamlit as st
 import joblib
+import numpy as np
 
-MODEL_PATH = "./models/LogisticRegressionModel.pkl"
-VECTORIZER_PATH = "./models/LogisticRegressionModel.pkl"
+MODEL_PATH = "./models/best_model.pkl"
+VECTORIZER_PATH = "./models/vectorizer.pkl"
 model = joblib.load(MODEL_PATH)
 vectorizer = joblib.load(VECTORIZER_PATH)
 
@@ -17,14 +18,30 @@ st.markdown(
     """
 )
 
+
+def preprocess_input(text, vectorizer):
+    tfidf_features = vectorizer.transform([text])
+    text_length = np.array([len(text.split())]).reshape(-1, 1)
+    features = np.hstack([tfidf_features.toarray(), text_length])
+
+    return features
+
+
 tweet_text = st.text_area("Enter the tweet text:", placeholder="Type your tweet here...")
 
 if st.button("Analyze Sentiment"):
     if tweet_text.strip():
-        tweet_vectorized = vectorizer.transform([tweet_text])
+        processed_features = preprocess_input(tweet_text, vectorizer)
 
-        prediction = model.predict(tweet_vectorized)[0]
+        prediction = model.predict(processed_features)[0]
 
-        st.success(f"Predicted Sentiment: {prediction}")
+        sentiment_map = {
+            0: "Negative",
+            1: "Neutral",
+            2: "Positive"
+        }
+        predicted_sentiment = sentiment_map[prediction]
+
+        st.success(f"Predicted Sentiment: {predicted_sentiment}")
     else:
         st.warning("Please enter a valid tweet text.")
